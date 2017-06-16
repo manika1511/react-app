9bb7c116ed09dc4c2b6dd46f1aa52abc3550cf9a
manika15 <manika.kapoor90@gmail.com>
2017-06-15 20:59:03 -0700

    node data display table

---
+++ src/components/Dashboard/node_table.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../../index.css';
+
+export default class TableDisplay extends React.Component{
+  render(){
+    return(
+      <div>
+      <h3>Nodes in Rack {this.props.rack}</h3>
+      <table>
+      <tr>
+      <th>Name</th>
+      <th>OS</th>
+      <th>Age</th>
+      </tr>
+      {
+        this.props.node_data.map(function(node_data) {
+          return (
+            <tr>
+              <td id={node_data[0]['name']}>{node_data[0]['name']} </td>
+              <td id={node_data[0]['OS']}>{node_data[0]['OS']} </td>
+              <td id={node_data[0]['age']}>{node_data[0]['age']} </td>
+            </tr>
+          )
+        })
+      }
+      </table>
+      </div>
+    );
+  }
+}
