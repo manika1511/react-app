c941463b8f8202f691c7087fb256c2e4a35ace69
manika15 <manika.kapoor90@gmail.com>
2017-06-16 03:13:59 -0700

    axios import removed

--- src/components/Dashboard/rack.js
+++ src/components/Dashboard/rack.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios';
-
 import {restClient} from './restClient';
 
 export default class RackInfo extends React.Component{
