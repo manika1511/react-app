9af12aac6fd43ba4bd67a35c6d4ef6cfe66c2353
manika15 <manika.kapoor90@gmail.com>
2017-06-18 12:23:17 -0700

    stylesheet reference added

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,9 +1,8 @@
-// src/components/App/index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
 import AZ_Dropdown from './az_dropdown';
 import RackInfo from './rack';
 import RackNodes from './rack_nodes'
+import '../../stylesheets/index.scss';
 
 export default class DashBoard extends React.Component{
   constructor(props){
@@ -27,13 +26,23 @@ export default class DashBoard extends React.Component{
     if (this.state.selectedValue !== "" && this.state.selectedRack === ""){
       return (
       <div>
+      <div>
+        <h1>On Boarding </h1>
+      </div>
       <AZ_Dropdown change={this.modifyDcState.bind(this)} />
       <RackInfo dc={this.state.selectedValue} selRack={this.modifyRackState.bind(this)}/>
       </div>
       );
     } else if (this.state.selectedValue !== "" && this.state.selectedRack !== ""){
       return (
+        <div>
+        <div>
+          <h1>On Boarding </h1>
+        </div>
+        <div>
         <RackNodes rack={this.state.selectedRack} />
+        </div>
+        </div>
       );
     }
   }
