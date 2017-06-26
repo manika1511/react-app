44af7315e2af87500a562a2b534525dfa0911b70
manika15 <manika.kapoor90@gmail.com>
2017-06-26 13:51:45 -0700

    added checkbox

--- src/components/Dashboard/node_table.js
+++ src/components/Dashboard/node_table.js
@@ -7,28 +7,27 @@ export default class TableDisplay extends React.Component{
     this.state={
       selectedNodes:[],
     };
+    this.addNode= this.addNode.bind(this)
   }
 
   addNode(node, checked){
+    console.log(this.state.selectedNodes)
     if (checked === true && this.state.selectedNodes.indexOf(node) == -1){
-      this.setState.selectedNodes({
+      this.setState({
         selectedNodes: this.state.selectedNodes.push(node)
       });
     }
     else if (checked === false){
       if (this.state.selectedNodes.indexOf(node) > -1){
-        this.setState.selectedNodes({
+        this.setState({
           selectedNodes: this.state.selectedNodes.splice(this.state.selectedNodes.indexOf(node),1)
         });
       }
     }
-    console.log(this.state.selectedNodes)
   }
 
   render(){
     console.log(this.state.selectedNodes)
-    this.addNode()
-    if (this.props.node_data){
     return(
       <div>
       <h3>Nodes in Rack {this.props.rack}</h3>
@@ -40,10 +39,10 @@ export default class TableDisplay extends React.Component{
       <th>Age</th>
       </tr>
       {
-          this.props.node_data.map(function(node_data) {
+          this.props.node_data.map((node_data) => {
           return (
             <tr>
-              <td><CheckBox node={node_data[0]['name']} /></td>
+              <td><CheckBox node={node_data[0]['name']} change={this.addNode}/></td>
               <td id={node_data[0]['name']}>{node_data[0]['name']} </td>
               <td id={node_data[0]['OS']}>{node_data[0]['OS']} </td>
               <td id={node_data[0]['age']}>{node_data[0]['age']} </td>
@@ -55,5 +54,4 @@ export default class TableDisplay extends React.Component{
       </div>
     );
   }
-}
 }
