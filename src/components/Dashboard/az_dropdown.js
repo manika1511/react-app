51c07159976494527c18c9a54575d245c6111ffe
manika15 <manika.kapoor90@gmail.com>
2017-06-30 13:04:26 -0700

    added css az_drop

--- src/components/Dashboard/az_dropdown.js
+++ src/components/Dashboard/az_dropdown.js
@@ -19,7 +19,7 @@ export default class AZ_Dropdown extends React.Component{
         return (
           <div>
           <div className="left-div"> Select a Data Center: </div>
-          <select value={this.state.selectValue} onChange={this.handleChange.bind(this)}>
+          <select className="az_drop" value={this.state.selectValue} onChange={this.handleChange.bind(this)}>
             <option value="CCG1">CCG1</option>
             <option value="CCG2">CCG2</option>
             <option value="CCG3">CCG3</option>
